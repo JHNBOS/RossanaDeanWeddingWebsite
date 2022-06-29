4a23cc2f56e3700d695aa159957c097f48a3a567
Johan Bos <bosbosjohan@gmail.com>
2022-06-29 21:03:16 +0200

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
 	production: true,
-	apiKey: 'AIzaSyA4LDD-8jmDRbZO9XwGNAm3pPu145VyVnM',
-	authDomain: 'rossana-and-deans-wedding.firebaseapp.com',
-	projectId: 'rossana-and-deans-wedding',
-	storageBucket: 'rossana-and-deans-wedding.appspot.com',
-	messagingSenderId: '843977363791',
-	appId: '1:843977363791:web:fb56d145368a600a89353a'
+	firebaseConfig: {
+		apiKey: 'AIzaSyA4LDD-8jmDRbZO9XwGNAm3pPu145VyVnM',
+		authDomain: 'rossana-and-deans-wedding.firebaseapp.com',
+		projectId: 'rossana-and-deans-wedding',
+		storageBucket: 'rossana-and-deans-wedding.appspot.com',
+		messagingSenderId: '843977363791',
+		appId: '1:843977363791:web:fb56d145368a600a89353a'
+	}
 };
