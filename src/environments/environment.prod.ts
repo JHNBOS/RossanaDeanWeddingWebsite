473785368782db6c37d99377a49ceb9e621b2d49
Johan Bos <bosbosjohan@gmail.com>
2022-07-02 13:59:00 +0200

    environment for production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: true,
+	firebaseConfig: {
+		apiKey: '{FIREBASE_API_KEY}',
+		authDomain: '{FIREBASE_DOMAIN}',
+		projectId: '{FIREBASE_PROJECT_ID}',
+		storageBucket: '{FIREBASE_STORAGE_BUCKET}',
+		messagingSenderId: '{FIREBASE_SENDER_ID}',
+		appId: '{IREBASE_APP_ID}'
+	}
+};
