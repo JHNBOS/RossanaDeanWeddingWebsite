a926f870fb88931bde801d92456ea3ab1a7daf99
Johan Bos <bosbosjohan@gmail.com>
2022-07-02 16:09:22 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
 	production: true,
 	firebaseConfig: {
-		apiKey: 'FIREBASE_API_KEY',
-		authDomain: 'FIREBASE_DOMAIN',
-		projectId: 'FIREBASE_PROJECT_ID',
-		storageBucket: 'FIREBASE_STORAGE_BUCKET',
-		messagingSenderId: 'FIREBASE_SENDER_ID',
-		appId: 'FIREBASE_APP_ID'
+		apiKey: 'AIzaSyA4LDD-8jmDRbZO9XwGNAm3pPu145VyVnM',
+		authDomain: 'rossana-and-deans-wedding.firebaseapp.com',
+		projectId: 'rossana-and-deans-wedding',
+		storageBucket: 'rossana-and-deans-wedding.appspot.com',
+		messagingSenderId: '843977363791',
+		appId: '1:843977363791:web:fb56d145368a600a89353a'
 	}
 };
