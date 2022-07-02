205d8d8777048293d384604162310dccc97dcef2
Johan Bos <bosbosjohan@gmail.com>
2022-07-02 15:41:36 +0200

    firebase env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
 	production: true,
 	firebaseConfig: {
-		apiKey: '{FIREBASE_API_KEY}',
-		authDomain: '{FIREBASE_DOMAIN}',
-		projectId: '{FIREBASE_PROJECT_ID}',
-		storageBucket: '{FIREBASE_STORAGE_BUCKET}',
-		messagingSenderId: '{FIREBASE_SENDER_ID}',
-		appId: '{IREBASE_APP_ID}'
+		apiKey: 'FIREBASE_API_KEY',
+		authDomain: 'FIREBASE_DOMAIN',
+		projectId: 'FIREBASE_PROJECT_ID',
+		storageBucket: 'FIREBASE_STORAGE_BUCKET',
+		messagingSenderId: 'FIREBASE_SENDER_ID',
+		appId: 'FIREBASE_APP_ID'
 	}
 };
