15212941c42fe36782475a92e76f5d91ac664420
Johan Bos <bosbosjohan@gmail.com>
2022-07-07 19:49:58 +0200

    remove font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './core/components/header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { registerLocaleData } from '@angular/common';
@@ -20,6 +19,7 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 
 import { environment } from '../environments/environment';
 import { MaterialModule } from './shared/material.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 registerLocaleData(localeEn, 'en');
 
@@ -29,9 +29,9 @@ registerLocaleData(localeEn, 'en');
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
-		NgbModule,
 		PagesModule,
 		MaterialModule,
+		FontAwesomeModule,
 		provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
 		provideFirestore(() => getFirestore()),
 		provideAuth(() => getAuth()),
