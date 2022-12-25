1e0debd5a0e397ca722541009d7f2b9eca877c19
Johan Bos <bosbosjohan@gmail.com>
2022-12-25 13:02:52 +0100

    Info page and translations

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,8 +1,8 @@
+import { TranslateService } from '@ngx-translate/core';
 import { AfterViewInit } from '@angular/core';
-// <reference types="@types/google.maps" />
-
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { markTimeline } from 'console';
 
 @Component({
 	selector: 'app-info',
@@ -11,14 +11,29 @@ import { GoogleMap } from '@angular/google-maps';
 })
 export class InfoComponent implements OnInit, AfterViewInit {
 	@ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
+	@ViewChild(MapInfoWindow, { static: false }) infoWindow!: MapInfoWindow;
 
-	public mapZoom: number = 15;
-	public mapCenter: google.maps.LatLngLiteral = {
-		lat: 51.32304604630722,
-		lng: 4.950825369325472
+	public mapZoom: number = 13;
+
+	public centerPosition: google.maps.LatLngLiteral = {
+		lat: 51.31485268187664,
+		lng: 4.989035077575983
 	};
 
-	public marker: google.maps.Marker;
+	public hotelPosition: google.maps.LatLng = new google.maps.LatLng({
+		lat: 51.32304604630722,
+		lng: 4.950825369325472
+	});
+
+	public venuePosition: google.maps.LatLng = new google.maps.LatLng({
+		lat: 51.31023875264583,
+		lng: 5.024826474643172
+	});
+
+	public hotelMarker!: google.maps.Marker;
+	public venueMarker!: google.maps.Marker;
+
+	public infoContent: string = '';
 
 	public mapOptions: google.maps.MapOptions = {
 		mapTypeId: 'roadmap',
@@ -29,18 +44,50 @@ export class InfoComponent implements OnInit, AfterViewInit {
 		minZoom: 8
 	};
 
-	constructor() {}
+	constructor(public translate: TranslateService) {}
 
 	ngOnInit(): void {
-		this.marker = new google.maps.Marker({
-			position: this.mapCenter,
-			label: '',
-			title: '',
-      animation: google.maps.Animation.DROP
+		this.hotelMarker = new google.maps.Marker({
+			position: this.hotelPosition,
+			label: 'A',
+			title: 'Corsendonk Turnova',
+			icon: 'https://maps.google.com/mapfiles/ms/micons/green-dot.png',
+			animation: google.maps.Animation.DROP
+		});
+
+		this.venueMarker = new google.maps.Marker({
+			position: this.venuePosition,
+			label: 'B',
+			title: 'Alta Ripa',
+			icon: 'https://maps.google.com/mapfiles/ms/micons/yellow-dot.png',
+			animation: google.maps.Animation.DROP
 		});
 	}
 
-	public onMapLoaded(map: google.maps.Map): void {}
+	public openInfoWindow(marker: MapMarker): void {
+		this.infoContent = this.getMarkerInfo(marker);
+		this.infoWindow.open(marker);
+	}
+
+	private getMarkerInfo(marker: MapMarker): string {
+		let content = '';
+
+		if (marker.getPosition()! == this.hotelPosition) {
+			this.translate.get('info.pickup-blog.locations.pickup').subscribe((v: string) => (content = v));
+		}
+		if (marker.getPosition()! == this.venuePosition) {
+			this.translate.get('info.pickup-blog.locations.venue').subscribe((v: string) => (content = v));
+		}
+
+		return `
+<div id="infoContent" class="p-2">
+  <h3 class="mb-1 fw-bold">${marker.getTitle()}</h3>
+  <p>
+    ${content}
+  </p>
+</div>
+    `;
+	}
 
 	ngAfterViewInit(): void {}
 }
